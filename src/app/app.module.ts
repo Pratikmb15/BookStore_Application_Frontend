6b2d40347b361ef8f802727b0be004858bb1b400
pratikbagul15 <pratikbagul1508@gmail.com>
2025-05-05 20:58:43 +0530

    [Pratik] Created Dashboard Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,13 @@ import { MatBadgeModule } from '@angular/material/badge';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './components/auth/auth.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
